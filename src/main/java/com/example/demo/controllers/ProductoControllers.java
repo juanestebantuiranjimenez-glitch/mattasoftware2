a7ff96b19f6af22e0d338ca8a4f8a5d593756b62
Juan Jose <juanjosevelasquezparra8@gmail.com>
2026-05-07 00:28:31 -0500

    Llenando el controlador de productos

--- src/main/java/com/example/demo/controllers/ProductoControllers.java
+++ src/main/java/com/example/demo/controllers/ProductoControllers.java
@@ -1,1 +1,20 @@
+package com.example.demo.controllers;
 
+import com.example.demo.models.Producto;
+import com.example.demo.services.ProductoServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/productos") // Esta será la dirección: localhost:8080/productos
+public class ProductoControllers {
+
+    @Autowired
+    private ProductoServices productoService;
+
+    @GetMapping("/listar")
+    public List<Producto> listar() {
+        return productoService.listarTodos();
+    }
+}
